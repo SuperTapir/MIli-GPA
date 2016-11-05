d77c8bfb0de6e78c6d85f48f70c59ce74d3533f3
LiuKaiLong <helloworld.lkl@gmail.com>
2016-11-05 18:52:44 +0800

    这是开始

---
+++ js/mi.js
@@ -0,0 +1,140 @@
+var colClassName = document.querySelector('#col-class-name');
+var colPoint = document.querySelector('#col-point');
+var colCredit = document.querySelector('#col-credit');
+var calculateBtn = document.querySelector('#calculate-btn');
+var addNewBtn = document.querySelector('#add-new-btn');
+var answearModel = document.querySelector('#calculateAnswear .modal-body p');
+var titleModel = document.querySelector('#calculateAnswear .modal-title');
+window.onload = function() {
+    addNewBtn.addEventListener('click', addNewItem, false);
+    calculateBtn.addEventListener('click', calculateGPA, false);
+    colPoint.addEventListener('focusout', testPointInput, false);
+    colCredit.addEventListener('focusout', testCreditInput, false);
+}
+
+function addNewItem() {
+    var item1 = document.createElement('input');
+    item1.type = 'text';
+    item1.className = 'form-control input-sm';
+    item1.placeholder = '课程' + (parseInt(colClassName.lastElementChild.placeholder.substr(-1, 1)) + 1)
+    var item2 = document.createElement('input');
+    item2.type = 'text';
+    item2.className = 'form-control input-sm';
+    var item3 = document.createElement('input');
+    item3.type = 'text';
+    item3.className = 'form-control input-sm';
+    colClassName.appendChild(item1);
+    colPoint.appendChild(item2);
+    colCredit.appendChild(item3);
+    event.preventDefault();
+}
+
+function getGpaFromPoint(point) {
+    var gpa = 0;
+    switch (true) {
+        case (point <= 100) && (point >= 90):
+            gpa = 4;
+            break;
+        case (point < 90) && (point >= 85):
+            gpa = 3.7;
+            break;
+        case (point < 85) && (point >= 82):
+            gpa = 3.3;
+            break;
+        case (point < 82) && (point >= 78):
+            gpa = 3;
+            break;
+        case (point < 78) && (point >= 75):
+            gpa = 2.7;
+            break;
+        case (point < 75) && (point >= 72):
+            gpa = 2.3;
+            break;
+        case (point < 72) && (point >= 68):
+            gpa = 2;
+            break;
+        case (point < 68) && (point >= 66):
+            gpa = 1.7;
+            break;
+        case (point < 66) && (point >= 64):
+            gpa = 1.5;
+            break;
+        case (point < 64) && (point >= 60):
+            gpa = 1;
+            break;
+        case (point < 60) && (point >= 0):
+            gpa = 0;
+            break;
+        default:
+            gpa = -1;
+            break;
+    }
+    return gpa;
+}
+
+function testPointInput(event) {
+    var e = arguments[0] || window.event,
+        target = e.srcElement ? e.srcElement : e.target;
+    //在这里是实际绑定的事件
+    if (target.nodeName == "INPUT") {
+        if (isNaN(parseInt(target.value)) || target.value < 0 || target.value > 100) {
+            target.value = '错误';
+        }
+    }
+}
+
+function testCreditInput(event) {
+    var e = arguments[0] || window.event,
+        target = e.srcElement ? e.srcElement : e.target;
+    //在这里是实际绑定的事件
+    if (target.nodeName == "INPUT") {
+        if (isNaN(parseInt(target.value)) || target.value < 0 || target.value > 10) {
+            target.value = '错误';
+        }
+    }
+}
+
+function getCreditArr() {
+    var creditInput = document.querySelectorAll('#col-credit input');
+    var arr = [];
+    for (var i = 0; i < creditInput.length; i++) {
+        arr.push(parseInt(creditInput[i].value, 10));
+    }
+    return arr;
+}
+
+function getPointArr() {
+    var pointInput = document.querySelectorAll('#col-point input');
+    var arr = [];
+    for (var i = 0; i < pointInput.length; i++) {
+        arr.push(parseInt(pointInput[i].value, 10));
+    }
+    return arr;
+
+}
+
+function calculateGPA(event) {
+    event.preventDefault();
+    var i = 0;
+    var flag = 1;
+    var up = 0;
+    var down = 0;
+    var pointArr = getPointArr();
+    var creditArr = getCreditArr();
+    for (i = 0; i < colClassName.children.length; i++) {
+        if (isNaN(parseInt(pointArr[i])) || isNaN(parseInt(creditArr[i]))) {
+            flag = 0;
+            break;
+        }
+        up += (getGpaFromPoint(pointArr[i]) * creditArr[i]);
+        down += creditArr[i];
+    }
+    if (flag == 0 || isNaN(((up / down).toFixed(2)))) {
+        titleModel.innerHTML = '查询失败';
+        answearModel.innerHTML = '请检查您的数据的有效性';
+    } else {
+        titleModel.innerHTML = '查询成功';
+        answearModel.innerHTML = '您的平均学分绩点为' + ((up / down).toFixed(2));
+    }
+    $('#calculateAnswear').modal('show');
+}
